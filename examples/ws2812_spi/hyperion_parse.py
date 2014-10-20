41a6c88bc097aab0ff5437880416f18945ec084b
Jürgen Weigert <juewei@fabfolk.com>
2014-10-20 21:11:00 +0200

    using select, to make it a nonblocking thing.

--- examples/ws2812_spi/hyperion_parse.py
+++ examples/ws2812_spi/hyperion_parse.py
@@ -7,7 +7,8 @@
 # Apply GPLv2.0 or ask.
 #
 
-import socket, sys, json
+import socket, select, sys, time, json
+
 HOST = ''   	# Symbolic name, meaning all available interfaces
 PORT = 19444	# Hyperion port
 
@@ -18,43 +19,57 @@ def parse_hyperion(data):
   """
   try:
     d = json.loads(data)
-    rgb = d['color']
-    print "r=%d, g=%d, b=%d" % (rgb[0], rgb[1], rgb[2])
   except Exception as e:
     print >>sys.stderr, "%s: bad json: '%s'\n" % (e, data)
+    return
+
+  if d:
+    if d.has_key('color'):
+      rgb = d['color']
+      print "r=%d, g=%d, b=%d" % (rgb[0], rgb[1], rgb[2])
+    else:
+      print "unknown json command: %s" % data
 
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 #Bind socket to local host and port
 try:
-    s.bind((HOST, PORT))
+    server.bind((HOST, PORT))
 except socket.error as msg:
     print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
     sys.exit()
 
-s.listen(5)
+server.listen(5)
+
+ss = {}
+ss[server.fileno()] = server
 
 
-# check if s or conn are redable without blocking:
-# r = select.select([s, conn], [], [], 0)
+def poll_socks(ss):
+    data = None
+    r = select.select(ss.values(), [], [], 0)[0]
+    for s in r:
+        if s == server:
+            conn, addr = s.accept()
+            print 'Connected with ' + addr[0] + ':' + str(addr[1])
+	    ss[conn.fileno()] = conn
+	else:
+	    data = s.recv(256)
+	    if not data:
+	        del(ss[s.fileno()])
+                print 'Closing fd=' + str(s.fileno())
+		s.close()
+	    else:
+	    	s.send("{\"success\": true}\n")
+    return data 
+
 
 while 1:
-    #wait to accept a connection - blocking call
-    print 'waiting...'
-    conn, addr = s.accept()
-    try:
-        print 'Connected with ' + addr[0] + ':' + str(addr[1])
-        while True:
-            data = conn.recv(256)
-	    # print >>sys.stderr, 'received "%s"' % data
-	    if not data:
-	        break
-	    parse_hyperion(data)
-	    conn.send("{\"success\": true}\n")
-    finally:
-        print 'Closing ' + addr[0] + ':' + str(addr[1])
-        conn.close()
+    data = poll_socks(ss)
+    if (data): parse_hyperion(data)
+    time.sleep(0.2)
+    print >> sys.stderr, ".",
 
-s.close()
+server.close()
